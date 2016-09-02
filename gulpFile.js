76dccfcdce8032d853b95dedf5c790a708bbd3ce
dstyle0210 <dstyle0210@gmail.com>
2016-09-02 13:31:55 +0900

    v0.0.2 업데이트. - 소스 최적화 - dist 시 줄바꿈처리 연결.

--- gulpFile.js
+++ gulpFile.js
@@ -1,8 +1,9 @@
 /**
- * Gulp Setting v0.0.1
+ * Gulp Setting v0.0.2
  * @ahther 디스타일(마봉아빠 , dstyle0210@gmail.com)
- * @url : http://dstyleitsme.tistory.com/entry/GulpSetting
+ * @url : https://dstyle0210.github.io/gulp-setting/
  */
+
 'use strict';
 
 var gulp = require('gulp');
@@ -15,33 +16,21 @@ var csscomb = require('gulp-csscomb');
 var insert = require('gulp-insert');
 var runSequence = require('run-sequence');
 var folders = require('gulp-folders');
-
-// 현재 안씀
 var replace = require('gulp-replace');
 
+// 현재 안씀
 
 
 // 환경설정
-var srcPath = {
-		root:"./src",
-		css:"./src/css",
-		scss:"./src/scss",
-		less:"./src/less",
-	};
-var distPath = {
-		root:"./dist",
-		css:"./dist/css"
-}
+var srcPath = {};
+srcPath.root = "./src";
+srcPath.css = srcPath.root+"/css";
+srcPath.scss = srcPath.root+"/scss";
+srcPath.less = srcPath.root+"/less";
 
-
-// 메소드 만들기
-function getFolder(file){
-	return path.parse( path.parse(file.path).dir ).base;
-};
-function getTimeStamp() {
-    var now = new Date();
-    return "["+(now.getHours() + ':' +((now.getMinutes() < 10)? ("0" + now.getMinutes()): (now.getMinutes())) + ':' +((now.getSeconds() < 10)? ("0" + now.getSeconds()): (now.getSeconds())))+"]";
-}
+var distPath = {};
+distPath.root = "./dist";
+distPath.css = distPath.root+"/css";
 
 
 /*! Default */
@@ -51,27 +40,6 @@ gulp.task("default",function(callback){
 gulp.task("dist",function(callback){
 	runSequence("css:dist",callback);
 });
-
-
-
-/*! 중복 pipe */
-function pipeLineScss(gulpFiles){
-	return gulpFiles.pipe(sass({indentType:"tab",indentWidth:1}).on('error', sass.logError))
-	.pipe(csscomb())
-	.pipe(gulp.dest(srcPath.css));
-};
-function pipeLineLess(gulpFiles){
-	return gulpFiles.pipe(less())
-	.pipe(csscomb())
-	.pipe(gulp.dest(srcPath.css));
-};
-function pipeLineConcatCSS(gulpFiles,folderName){
-	return gulpFiles.pipe(concat(folderName))
-    .pipe(insert.prepend('@charset "UTF-8";\n'))
-    .pipe(gulp.dest(srcPath.css));
-};
-
-
 gulp.task("build",function(callback){
 	runSequence("scss:build","less:build","css:concat",callback);
 });
@@ -87,14 +55,11 @@ gulp.task("less",function(callback){
 gulp.task("css",function(callback){
 	runSequence("css:concat","css:watch",callback);
 });
-
-
 gulp.task("scss:build",function(){
 	return pipeLineScss( gulp.src(srcPath.scss+"/**/*.scss",{"base":srcPath.scss}) );
 });
 gulp.task("less:build",function(){
 	return pipeLineLess( gulp.src(srcPath.less+"/**/*.less",{"base":srcPath.less}) );
-	
 });
 gulp.task("css:concat", folders(srcPath.css, function(folder){
     return pipeLineConcatCSS( gulp.src(path.join(srcPath.css, folder, '*.css')) , folder + '.css' );
@@ -106,7 +71,6 @@ gulp.task("scss:watch",function(){
 		console.log(getTimeStamp() + " [sass:watch] "+name+" changed");
 	});
 });
-/*! LESS watch */
 gulp.task("less:watch",function(){
 	return gulp.watch(srcPath.less+"/**/*.less").on("change",function(file){
 		var name = path.parse(file.path).base;
@@ -114,7 +78,6 @@ gulp.task("less:watch",function(){
 		console.log(getTimeStamp() + " [less:watch] "+name+" changed");
 	});
 });
-/*! CSS Watch */
 gulp.task("css:watch",function(){
 	return gulp.watch([srcPath.css+"/**/*.css","!"+srcPath.css+"/*.css"]).on("change",function(file){
 		var folder = getFolder(file);
@@ -122,9 +85,40 @@ gulp.task("css:watch",function(){
 		console.log(getTimeStamp() + " [css:watch] "+folder+".css concated");
 	});
 });
-/*! dist : CSS */
 gulp.task("css:dist",function(){
 	gulp.src(srcPath.css+"/*.css")
 	.pipe(csso())
+    .pipe(replace(/}/g,'}\n'))
+    .pipe(replace('/*!','\n/*!'))
+    .pipe(replace('{.','{\n\t.'))
+    .pipe(replace('"UTF-8";','"UTF-8";\n'))
 	.pipe(gulp.dest(distPath.css));
 });
+
+
+
+// 메소드 만들기
+function getFolder(file){
+    return path.parse( path.parse(file.path).dir ).base;
+};
+function getTimeStamp() {
+    var now = new Date();
+    return "["+(now.getHours() + ':' +((now.getMinutes() < 10)? ("0" + now.getMinutes()): (now.getMinutes())) + ':' +((now.getSeconds() < 10)? ("0" + now.getSeconds()): (now.getSeconds())))+"]";
+}
+
+/*! 중복 pipe */
+function pipeLineScss(gulpFiles){
+    return gulpFiles.pipe(sass({indentType:"tab",indentWidth:1}).on('error', sass.logError))
+        .pipe(csscomb())
+        .pipe(gulp.dest(srcPath.css));
+};
+function pipeLineLess(gulpFiles){
+    return gulpFiles.pipe(less())
+        .pipe(csscomb())
+        .pipe(gulp.dest(srcPath.css));
+};
+function pipeLineConcatCSS(gulpFiles,folderName){
+    return gulpFiles.pipe(concat(folderName))
+        .pipe(insert.prepend('@charset "UTF-8";\n'))
+        .pipe(gulp.dest(srcPath.css));
+};
